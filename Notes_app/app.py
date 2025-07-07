14def0aeabfb3a1885c9176427d53de60c359f76
rmg <rmgajdosik@gmail.com>
2025-07-07 17:28:38 +0200

    Completed

--- Notes_app/app.py
+++ Notes_app/app.py
@@ -1,1 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+notes = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', notes=notes)
+
+
+@app.route('/add_note', methods=['GET', 'POST'])
+def add_note():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        content = request.form.get('content')
+        if title and content:
+            notes.append({'title': title, 'content': content})
+        return redirect(url_for('index'))
+    return render_template('add_note.html')
